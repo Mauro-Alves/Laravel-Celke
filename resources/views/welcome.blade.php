018ca9d3950246d5562d7b971517e42165b52da6
MauroAlves <mcbalves@gmail.com>
2024-04-30 10:31:16 -0300

    Projeto base

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel - Celke</title>
+    </head>
+
+    <body>
+
+        <h1>Bem-vindo</h1>
+
+        <!-- <p>Data Atual: {{ \Carbon\Carbon::now() -> format('d/m/y H:i:s') }}</p> -->
+
+    </body>
+</html>
