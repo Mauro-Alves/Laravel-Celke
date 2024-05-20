231d24c7eebb1fb8a508fd6d875125e3e8327b41
MauroAlves <mcbalves@gmail.com>
2024-05-20 12:12:20 -0300

    Como incluir Bootstrap no Laravel

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie-edge">
+
+    <link href="{{ asset('css/bootstrap.min.css')}}" rel="stylesheet" >
+
     <title>Laravel Celke</title>
 </head>
 <body>
 
 @yield('content')
 
-
+<script src="{{ asset('js/bootstrap.bundle.min.js')}}"></script>
 
 </body>
 </html>
