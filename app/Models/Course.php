e3d7200c7097b83d3c3cf0eb54b8f22ee6267eb9
MauroAlves <mcbalves@gmail.com>
2024-05-20 11:44:08 -0300

    Como criar auditoria com Laravel

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -5,9 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Course extends Model
+use \OwenIt\Auditing\Auditable as AuditingAuditable;
+use OwenIt\Auditing\Contracts\Auditable;
+
+class Course extends Model implements Auditable
 {
-    use HasFactory;
+    use HasFactory, AuditingAuditable;
 
     // Indicar o nome da tabela
     protected $table = 'courses';
